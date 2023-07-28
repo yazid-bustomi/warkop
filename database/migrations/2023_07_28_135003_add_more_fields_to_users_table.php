c4477314177fc338b7e80d740ab48dc0f1aff08f
jav <jafarc@mhs.stmik-yadika.ac.id>
2023-07-28 22:21:16 +0700

    login bawaan breeze berhasil diubah ke username

--- database/migrations/2023_07_28_135003_add_more_fields_to_users_table.php
+++ database/migrations/2023_07_28_135003_add_more_fields_to_users_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username');
+            $table->string('email')->nullable()->change();
         });
     }
 
@@ -26,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,5 @@
 <x-guest-layout>
+    
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -7,6 +8,12 @@
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+         <!-- Name -->
+         <div class="mt-4">
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" required />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
