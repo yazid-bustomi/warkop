1f78d1769b169985cf39e6c1facf93e571b305fc
Bustomi <bustomi024@gmail.com>
2023-08-03 14:05:12 +0700

    memperbaiki tampilan product

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,11 +1,17 @@
-<x-app-layout>
+@extends('layouts/app')
+@section('konten')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="card">
                         <div class="card-body ">
-                            <a href="{{ route('admin.product.create') }}" class="btn btn-primary p-6">Tambah Product</a>
+                            <div class="row justify-content-between">
+
+                                <div class="div h3">Product</div>
+                                <a href="{{ route('admin.product.create') }}" class="btn btn-primary mb-3 float-right">Tambah
+                                    Product</a>
+                            </div>
                             <table class="table table-striped ">
                                 <thead>
                                     <tr>
@@ -22,10 +28,11 @@
                                             <th scope="row">{{ $i }}</th>
                                             <td>{{ $data->name }}</td>
                                             <td>{{ $data->price }}</td>
+                                            <td>Kosong</td>
                                         </tr>
                                         <?php $i++; ?>
                                     @endforeach
-                    
+
                                 </tbody>
                             </table>
                         </div>
@@ -34,4 +41,4 @@
                 </div>
             </div>
         </div>
-</x-app-layout>
+    @endsection
